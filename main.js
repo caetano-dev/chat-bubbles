7ccdc1341c9d2103138f07ad1580003388d507cf
drull <pcaetanop@protonmail.com>
2023-10-28 20:49:07 -0300

    Send message with Enter key

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-let messageArray = []
+const messageArray = []
 const chatContainer = document.getElementById("chatContainer")
 
 function sendMessage(){
-    const userInput = document.getElementById("userInput").value
+  const userInput = document.getElementById("userInput").value
+  if (userInput !== ""){
 
     messageArray.push(userInput)
     document.getElementById("userInput").value = ""
@@ -10,23 +11,32 @@ function sendMessage(){
     displayMessages()
 
     setTimeout(()=> {
-        if (messageArray.length > 0){
-            messageArray.shift();
-            displayMessages();
-        }
+      if (messageArray.length > 0){
+        messageArray.shift();
+        displayMessages();
+      }
     }, 10000)
+  }
 }
 
 function displayMessages(){
-    chatContainer.innerHTML = ""
-    messageArray.forEach((message) => {
-        const chatBubble = document.createElement("div")
-        chatBubble.textContent = message
-        chatBubble.className = "chat-bubble"
-        chatContainer.appendChild(chatBubble)
-        setTimeout(()=>{
-            chatContainer.removeChild(chatBubble)
-        }, 10000)
-        
-    });
+  chatContainer.innerHTML = ""
+  for (message of messageArray) {
+    const chatBubble = document.createElement("div")
+    chatBubble.textContent = message
+    chatBubble.className = "chat-bubble"
+    chatContainer.appendChild(chatBubble)
+    setTimeout(()=>{
+      chatContainer.removeChild(chatBubble)
+    }, 10000)
+
+  };
 }
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    console.log(event.key)
+    sendMessage();
+  }
+});
+
